d8ae046c83ddef69f0553d4c0c08cd4c01c7ab51
Cameron Smart <smartcameron@gmail.com>
2021-12-10 01:35:23 -0800

    Delete scry.py

--- src/scry.py
+++
@@ -1,9 +0,0 @@
-import os
-
-async def scry(path, results=[]):
-    for filename in os.listdir(path):
-        if 'posts' not in filename and '.' not in filename:
-            return await scry(path+'/'+filename, results)
-        else:
-            results.append(filename)
-    return results
